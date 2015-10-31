13a57ae49c7ea83418b5dd3e7d51523c900bb982
Igor S. Morozov <moroz@fastmail.com>
2015-10-31 23:46:12 +0300

    Изменена структура модуля, добавлен внешний тактовый сигнал для мастера

--- ADC.v
+++ ADC.v
@@ -1,2 +1,15 @@
-module ADC(in, out, sclk, reset);
+module ADC(in_slave, out_slave, out, cs, sclk, reset, clk, PM, add, shadow, seq);
+	parameter DIGITS = 8; // 8 for AD7908
+	input in_slave;
+	input reset;
+	input clk;
+	input [2:0] add;
+	input shadow;
+	input seq;
+	input [1:0] PM;
+	output out_slave; 
+	output cs;
+	output sclk; 
+	output [DIGITS-1:0] out;
+	
 endmodule
